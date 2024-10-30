9eade2226e6ad117296e88b2357584565d5a367f
lariprado7 <168583530+lariprado7@users.noreply.github.com>
2024-10-30 16:47:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,107 @@
+criaCartao(
+    'Romance Lésbico | Carol',
+    'Qual é o nome da jovem fotógrafa que se apaixona por Carol?',
+    'Therese Belivet.'
+)
+
+criaCartao(
+    'Romance Lésbico | Light Light Light',
+    'Qual é o tema central do filme "Light Light Light"?',
+    'A relação amorosa entre duas mulheres.'
+)
+
+criaCartao(
+    'Romance Lésbico | Você Pode Viver Para Sempre',
+    'Como a protagonista lida com sua sexualidade em "Você Pode Viver Para Sempre"?',
+    'Ela enfrenta desafios em um ambiente conservador.'
+)
+
+criaCartao(
+    'Romance Lésbico | Imagine Eu e Você',
+    'O que a protagonista percebe durante seu casamento em "Imagine Eu e Você"?',
+    'Seus sentimentos por outra mulher.'
+)
+
+criaCartao(
+    'Romance Lésbico | Carol',
+    'Em que década se passa a história de "Carol"?',
+    'Nos anos 1950.'
+)
+
+criaCartao(
+    'Romance Lésbico | Imagine Eu e Você',
+    'Qual é o nome da personagem que se sente atraída por Luce em "Imagine Eu e Você"?',
+    'Rachel.'
+)
+
+criaCartao(
+    'Romance Hétero | 10 Coisas que Eu Odeio em Você',
+    'Qual é o nome da protagonista que recita um poema no final do filme?',
+    'Kat Stratford.'
+)
+
+criaCartao(
+    'Romance Hétero | Como Perder um Homem em 10 Dias',
+    'Qual é a aposta que Andie faz em "Como Perder um Homem em 10 Dias"?',
+    'Fazer um homem se apaixonar por ela em 10 dias.'
+)
+
+criaCartao(
+    'Romance Hétero | Brilho Eterno de uma Mente Sem Lembranças',
+    'Qual é o nome do procedimento que Joel e Clementine fazem para apagar suas memórias?',
+    'O procedimento de apagamento de memória.'
+)
+
+criaCartao(
+    'Romance Hétero | (500) Dias com Ela',
+    'Qual é a profissão de Tom Hansen em "(500) Dias com Ela"?',
+    'Ele é um designer de cartões.'
+)
+
+criaCartao(
+    'Romance Hétero | Brilho Eterno de uma Mente Sem Lembranças',
+    'Quem são os protagonistas do filme "Brilho Eterno de uma Mente Sem Lembranças"?',
+    'Joel Barish e Clementine Kruczynski.'
+)
+
+criaCartao(
+    'Romance Hétero | (500) Dias com Ela',
+    'Qual é o principal tema explorado em "(500) Dias com Ela"?',
+    'A desilusão amorosa e a percepção do amor.'
+)
+
+criaCartao(
+    'Comédia | Gente Grande 2',
+    'Qual é o evento que os amigos se reúnem para celebrar em "Gente Grande 2"?',
+    'O 40º aniversário de Lenny.'
+)
+
+criaCartao(
+    'Comédia | Shrek',
+    'Qual é o nome do burro que se torna amigo de Shrek?',
+    'Donkey.'
+)
+
+criaCartao(
+    'Comédia | Minha Mãe é Uma Peça',
+    'Quem é a protagonista de "Minha Mãe é Uma Peça"?',
+    'Dona Hermínia.'
+)
+
+criaCartao(
+    'Comédia | Meninas Malvadas',
+    'Qual é o nome da líder das "Plastics" em "Meninas Malvadas"?',
+    'Regina George.'
+)
+
+criaCartao(
+    'Comédia | Shrek',
+    'O que Shrek e Fiona devem fazer para se tornar verdadeiramente felizes no final do primeiro filme?',
+    'Eles devem se aceitar como são e se casarem.'
+)
+
+criaCartao(
+    'Comédia | Meninas Malvadas',
+    'Qual é o nome da nova estudante que se torna parte do grupo de "Plastics" em "Meninas Malvadas"?',
+    'Cady Heron.'
+)
